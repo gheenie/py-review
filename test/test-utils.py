eae077608519d5ecc33a7e78105fce1878899904
infoomics <sbx_94@hotmail.com>
2023-03-03 11:28:39 +0000

    add first test for happiest path

---
+++ test/test-utils.py
@@ -0,0 +1,29 @@
+from src.utils import (format_departments)
+
+def test_input_with_correct_shape_and_data():
+    """Happiest path where the input is 'correct'.
+
+    Input a list of dictionaries, with
+    each dictionary containing a 'department' key.
+    Expect a list of list, with the value of 'department'
+    in each second-level list.
+    """
+
+    input = [
+        {
+            'staff_id': 1,
+            'first_name': 'Duncan',
+            'last_name': 'Crawley',
+            'department': 'Beauty'
+        },
+        {
+            'staff_id': 2,
+            'first_name': 'Cat',
+            'last_name': 'Hoang',
+            'department': 'Footwear'
+        }
+    ]
+
+    expected = [['Beauty'], ['Footwear']]
+
+    assert format_departments(input) == expected
