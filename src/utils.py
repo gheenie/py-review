d485de91d76cee26e0b4f7c560c53f2e759a44a4
infoomics <sbx_94@hotmail.com>
2023-03-03 13:10:20 +0000

    add function docstring

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,25 @@ and return them in a simpler data structure.
 
 
 def format_departments(staffs):
+    """Get unique 'department' values from a List of staff Dicts.
+
+    Get the 'department' values from a list of dicts representing each staff, 
+    make them unique,
+    and return them as one list of lists representing each unique department.
+
+    Args:
+        staffs:
+            A list of dicts. Each dict is mapped to a staff row
+            from the queried database.
+            Each key of each dict is mapped to a staff column
+            from the queried database.
+
+    Returns:
+        A list of departments.
+        Each department is represented as a list of a string.
+        Items of the top-level list are unique.
+    """
+
     unique_departments = []
 
     for staff in staffs:
