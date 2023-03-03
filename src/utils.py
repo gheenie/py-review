d2a9118d4b16aef49e566865d7448f307d9320ce
infoomics <sbx_94@hotmail.com>
2023-03-03 11:27:56 +0000

    add first module-level docstring

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+"""Contains utility functions that formats raw data for database insertion.
+
+The incoming data will usually contain too much data 
+to be inserted into a database in one go.
+These utility functions will select smaller bits of data
+and return them in a simpler data structure.
+"""
+
+def format_departments():
+    pass
