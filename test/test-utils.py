8cec5dd40275a753a2eb1da2b0a66cff418ff106
infoomics <sbx_94@hotmail.com>
2023-03-03 11:35:24 +0000

    add test for function purity

--- test/test-utils.py
+++ test/test-utils.py
@@ -1,5 +1,6 @@
 from src.utils import (format_departments)
 
+
 def test_input_with_correct_shape_and_data():
     """Happiest path where the input is 'correct'.
 
@@ -27,3 +28,39 @@ def test_input_with_correct_shape_and_data():
     expected = [['Beauty'], ['Footwear']]
 
     assert format_departments(input) == expected
+
+
+def test_input_not_mutated():
+    input = [
+        {
+            'staff_id': 1,
+            'first_name': 'Duncan',
+            'last_name': 'Crawley',
+            'department': 'Beauty'
+        },
+        {
+            'staff_id': 2,
+            'first_name': 'Cat',
+            'last_name': 'Hoang',
+            'department': 'Footwear'
+        }
+    ]
+    expected = input = [
+        {
+            'staff_id': 1,
+            'first_name': 'Duncan',
+            'last_name': 'Crawley',
+            'department': 'Beauty'
+        },
+        {
+            'staff_id': 2,
+            'first_name': 'Cat',
+            'last_name': 'Hoang',
+            'department': 'Footwear'
+        }
+    ]
+
+    output = format_departments(input)
+
+    assert input is not output
+    assert input == expected
